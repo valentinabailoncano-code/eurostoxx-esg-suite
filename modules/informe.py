6a1fabe963c056966b3e6428c4b323a9d7ffe6eb
Valentina Bailon Cano <val34.bailon@gmail.com>
2025-06-27 14:36:13 +0200

    Cambio temporal

--- modules/informe.py
+++ modules/informe.py
@@ -45,7 +45,7 @@ Incluye:
 - Value at Risk y posibles escenarios futuros
 """
 
-                client = OpenAI()
+                client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
                 response = client.chat.completions.create(
                     model="gpt-3.5-turbo",
